ed32133141c4ba1dde3f2b34deced3e6e1ef86bd
jokerZ <429094465@qq.com>
2016-08-13 17:09:17 +0800

    添加工具

--- app/src/main/java/com/joker/gankor/utils/ImageUtil.java
+++ app/src/main/java/com/joker/gankor/utils/ImageUtil.java
@@ -2,6 +2,7 @@ package com.joker.gankor.utils;
 
 import android.widget.ImageView;
 
+import com.joker.gankor.R;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -23,12 +24,32 @@ public class ImageUtil {
         return instance;
     }
 
-//    显示图片
+    //    显示图片
     public void displayImage(String url, ImageView view) {
         DisplayImageOptions options = new DisplayImageOptions.Builder()
                 .cacheInMemory(true)
                 .cacheOnDisk(true)
                 .build();
         mImageLoader.displayImage(url, view, options);
+    }
+
+//    添加加载时显示图片，可有效解决快速滑动 recyclerView 异步加载图片错位
+    public void displayImageOnLoading(String url, ImageView view) {
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .showImageOnLoading(R.drawable.download_defualt)
+                .build();
+        mImageLoader.displayImage(url, view, options);
+    }
+
+    //    取出缓存图片路径
+    public String getAbsolutePath(String url) {
+        return mImageLoader.getDiskCache().get(url).getAbsolutePath();
+    }
+
+    //    判断是否有缓存
+    public boolean isExist(String url) {
+        return mImageLoader.getDiskCache().get(url).exists();
     }
 }
--- app/src/main/java/com/joker/gankor/utils/LazyUtil.java
+++ app/src/main/java/com/joker/gankor/utils/LazyUtil.java
@@ -3,8 +3,11 @@ package com.joker.gankor.utils;
 import android.content.Context;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringBufferInputStream;
 
 /**
  * Created by joker on 2016/8/12.
@@ -12,6 +15,7 @@ import java.io.IOException;
 public class LazyUtil {
     private static Toast toast;
 
+//    toast 优化显示
     public static void showToast(Context context, String content) {
         if (toast == null) {
             toast = Toast.makeText(context, content, Toast.LENGTH_SHORT);
@@ -21,11 +25,31 @@ public class LazyUtil {
         toast.show();
     }
 
+//    Closeable close() 方法封装，增强代码可读性
     public static void close(Closeable closeable) {
         try {
             closeable.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+//    CacheUtil 中的源码
+    public static String json2String(String json) {
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(new StringBufferInputStream(json)));
+            String readString = "";
+            String currentLine;
+            while ((currentLine = in.readLine()) != null) {
+                readString += currentLine;
+            }
+            return readString;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            close(in);
         }
     }
 }
