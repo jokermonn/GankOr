463b2e7abdc53d89ca4d8916336362b893c3ba4d
jokerZ <429094465@qq.com>
2016-08-16 08:21:17 +0800

    下拉刷新 RV

---
+++ app/src/main/java/com/joker/gankor/view/PullLoadRecyclerView.java
@@ -0,0 +1,74 @@
+package com.joker.gankor.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.AttributeSet;
+
+/**
+ * Created by joker on 2016/8/15.
+ */
+public class PullLoadRecyclerView extends RecyclerView {
+
+    private int mLastVisiblePosition;
+    private boolean isLoading = false;
+    private PullLoadRecyclerView.onPullLoadListener onPullLoadListener;
+
+    public PullLoadRecyclerView(Context context) {
+        super(context);
+    }
+
+    public PullLoadRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PullLoadRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void onScrolled(int dx, int dy) {
+        super.onScrolled(dx, dy);
+
+        if (getLayoutManager() instanceof StaggeredGridLayoutManager) {
+            StaggeredGridLayoutManager manager = (StaggeredGridLayoutManager) getLayoutManager();
+            int[] position = manager.findLastCompletelyVisibleItemPositions(new
+                    int[manager
+                    .getSpanCount()]);
+            mLastVisiblePosition = getMaxPosition(position);
+        } else if (getLayoutManager() instanceof LinearLayoutManager) {
+            LinearLayoutManager manager = (LinearLayoutManager) getLayoutManager();
+            mLastVisiblePosition = manager.findLastCompletelyVisibleItemPosition();
+        }
+
+        if (onPullLoadListener != null && mLastVisiblePosition + 1 == getAdapter().getItemCount() &&
+                !isLoading && dy > 0) {
+            isLoading = true;
+            onPullLoadListener.onPullLoad();
+        }
+    }
+
+    public int getMaxPosition(int[] positions) {
+        int size = positions.length;
+        int maxPosition = Integer.MIN_VALUE;
+        for (int i = 0; i < size; i++) {
+            maxPosition = Math.max(maxPosition, positions[i]);
+        }
+        return maxPosition;
+    }
+
+//    上拉加载更多接口
+    public void setPullLoadListener(onPullLoadListener onPullLoadListener) {
+        this.onPullLoadListener = onPullLoadListener;
+    }
+
+    public interface onPullLoadListener {
+        void onPullLoad();
+    }
+
+    public void setIsLoading(boolean isLoading) {
+        this.isLoading = isLoading;
+    }
+}
