72450607f5f1745b902e064e4efd0d0ec391f64f
jokerZ <429094465@qq.com>
2016-08-18 13:19:09 +0800

    添加修改资源文件

--- app/src/main/java/com/joker/gankor/GankOrApplication.java
+++ app/src/main/java/com/joker/gankor/GankOrApplication.java
@@ -5,9 +5,6 @@ import android.content.Context;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.utils.StorageUtils;
-
-import java.io.File;
 
 /**
  * Created by joker on 2016/8/4.
@@ -20,7 +17,6 @@ public class GankOrApplication extends Application {
     }
 
     private void initImageLoader(Context context) {
-        File cacheDir = StorageUtils.getCacheDirectory(context);
         ImageLoaderConfiguration configuration = ImageLoaderConfiguration.createDefault(context);
         ImageLoader.getInstance().init(configuration);
     }
